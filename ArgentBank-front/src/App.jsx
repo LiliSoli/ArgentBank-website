5a1e0ed58e189d5d677b086a46e1ce208c9ff28c
Aurélie Solier <solier.lili@gmail.com>
2024-08-19 22:51:42 +0200

    First commit Première architecture components

---
+++ ArgentBank-front/src/App.jsx
@@ -0,0 +1,24 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn'; 
+import User from '.pages/User'
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<SignIn />} />
+          <Route path="/about" element={<User />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
